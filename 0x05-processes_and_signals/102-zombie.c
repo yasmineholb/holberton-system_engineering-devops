50dd211fe3192799d2878653211f3ab1d26d9bb5
yasmineholb <1055@holbertonschool.com>
2020-02-10 20:15:40 +0100

    zombie process

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -3,18 +3,6 @@
 #include <unistd.h>
 #include <stdlib.h>
 /**
-*infinite_while - fn
-*Return: int
-*/
-int infinite_while(void)
-{
-while (1)
-{
-sleep(1);
-}
-return (0);
-}
-/**
 *main - fn
 *Return: int
 */
