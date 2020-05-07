24ae53e6e4713d7c7827d67167e2be5d8d22a092
yasmineholb <1055@holbertonschool.com>
2020-05-07 19:57:30 +0200

    test2

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,7 +8,7 @@ def top_ten(subreddit):
 
     ur = requests.get("https://www.reddit.com/r/{}/hot.json?limit=10"
                       .format(subreddit),
-                      headers={"User-Agent": "My-User-Agent"})
+                      headers={"User-Agent": "User-Agent"})
     if ur.status_code == 200:
         [print(t.get("data").get("title"))
          for t in ur.json().get("data").get("children")]
