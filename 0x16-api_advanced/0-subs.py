cdc6b220b6c20d2a09c841d749b9a05a69dd718a
yasmineholb <1055@holbertonschool.com>
2020-05-07 19:56:23 +0200

    test2

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,8 +7,7 @@ def number_of_subscribers(subreddit):
     """returns the number of subscribers"""
     ur = requests.get("https://www.reddit.com/r/{}/about.json"
                       .format(subreddit),
-                      headers={"User-Agent": "User-Agent"},
-                      allow_redirects=False)
+                      headers={"User-Agent": "User-Agent"})
     if ur.status_code == 200:
         return ur.json().get("data").get("subscribers")
     return 0
