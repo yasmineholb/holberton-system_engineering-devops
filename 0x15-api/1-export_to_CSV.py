cfa387aa40761add7921075ad84d8d49da3df8d7
yasmineholb <1055@holbertonschool.com>
2020-05-31 01:36:15 +0200

    csv

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Python script that, for a given employee ID, returns information """
+import requests
+import sys
+import csv
+url = "https://jsonplaceholder.typicode.com"
+userId = int(sys.argv[1])
+user = requests.get(url + "/users/{}".format(userId))
+td = requests.get(url + '/todos')
+name = user.json().get('username')
+file = sys.argv[1] + '.csv'
+with open(file, mode='w') as f:
+    writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL,
+                        lineterminator='\n')
+    for t in td.json():
+        writer.writerow([userId, name, str(t.get('completed')),
+                        t.get('title')])
