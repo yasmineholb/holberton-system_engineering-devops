d3b66be28559597dae03f59bfeff2d5b6fd94877
yasmineholb <1055@holbertonschool.com>
2020-05-31 01:47:28 +0200

    csv

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -3,15 +3,17 @@
 import requests
 import sys
 import csv
-url = "https://jsonplaceholder.typicode.com"
-userId = int(sys.argv[1])
-user = requests.get(url + "/users/{}".format(userId))
-td = requests.get(url + '/todos')
-name = user.json().get('username')
-file = sys.argv[1] + '.csv'
-with open(file, mode='w') as f:
-    writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL,
-                        lineterminator='\n')
-    for t in td.json():
-        writer.writerow([userId, name, str(t.get('completed')),
-                        t.get('title')])
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com"
+    userId = int(sys.argv[1])
+    user = requests.get(url + "/users/{}".format(userId))
+    td = requests.get(url + '/todos')
+    name = user.json().get('username')
+    file = sys.argv[1] + '.csv'
+    with open(file, mode='w') as f:
+        writer = csv.writer(f, delimiter=',', quotechar='"',
+                            quoting=csv.QUOTE_ALL,
+                            lineterminator='\n')
+        for t in td.json():
+            writer.writerow([userId, name, str(t.get('completed')),
+                            t.get('title')])
